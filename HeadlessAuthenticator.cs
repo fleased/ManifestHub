d72a022e226df3a11bb9e095972cc22c61bb32e3
ikun0014 <ikun0014@qq.com>
2025-01-26 21:33:07 +0800

    Update HeadlessAuthenticator.cs

--- HeadlessAuthenticator.cs
+++ HeadlessAuthenticator.cs
@@ -7,17 +7,17 @@ public class HeadlessAuthenticator(AccountInfoCallback account) : IAuthenticator
     private AccountInfoCallback _account = account;
 
     public Task<bool> AcceptDeviceConfirmationAsync() {
-        Console.Error.WriteLine($"STEAM GUARD! Pending device confirmation for {_account.AccountName}...");
+        Console.Error.WriteLine($"检测到STEAM GUARD，尝试向 {_account.AccountName} 的设备发送验证请求...");
         return Task.FromResult(true);
     }
 
     public Task<string> GetDeviceCodeAsync(bool previousCodeWasIncorrect) {
         return Task.FromException<string>(
-            new AuthenticationException("Authentication failed", EResult.AccountLoginDeniedNeedTwoFactor));
+            new AuthenticationException("验证失败", EResult.AccountLoginDeniedNeedTwoFactor));
     }
 
     public Task<string> GetEmailCodeAsync(string email, bool previousCodeWasIncorrect) {
         return Task.FromException<string>(
-            new AuthenticationException("Authentication failed", EResult.AccountLogonDeniedVerifiedEmailRequired));
+            new AuthenticationException("验证失败", EResult.AccountLogonDeniedVerifiedEmailRequired));
     }
 }
